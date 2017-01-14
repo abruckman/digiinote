102f0becaafb907643892cbd85f858e3a6759b9e
Andrew Bruckman <abruckman09@gmail.com>
2017-01-14 15:18:10 -0800

    moved whiteboard for test

---
+++ app/controllers/checks_controller.rb
@@ -0,0 +1,84 @@
+class ChecksController < ApplicationController
+  before_action :set_check, only: [:show, :edit, :update, :destroy]
+
+  # GET /checks
+  # GET /checks.json
+  def index
+    @checks = Check.all
+  end
+
+  # GET /checks/1
+  # GET /checks/1.json
+  def show
+    file_name = "/whiteboard.JPG"
+
+# Performs label detection on the image file
+    labels = VISION.image(file_name).text
+
+    p labels.text
+  end
+
+  # GET /checks/new
+  def new
+    @check = Check.new
+  end
+
+  # GET /checks/1/edit
+  def edit
+  end
+
+  # POST /checks
+  # POST /checks.json
+  def create
+    @check = Check.new(check_params)
+
+    p image = Vision::Image.new()
+
+    p vision.to_json
+
+    respond_to do |format|
+      if @check.save
+        format.html { redirect_to @check, notice: 'Check was successfully created.' }
+        format.json { render :show, status: :created, location: @check }
+      else
+        format.html { render :new }
+        format.json { render json: @check.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /checks/1
+  # PATCH/PUT /checks/1.json
+  def update
+    respond_to do |format|
+      if @check.update(check_params)
+        format.html { redirect_to @check, notice: 'Check was successfully updated.' }
+        format.json { render :show, status: :ok, location: @check }
+      else
+        format.html { render :edit }
+        format.json { render json: @check.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /checks/1
+  # DELETE /checks/1.json
+  def destroy
+    @check.destroy
+    respond_to do |format|
+      format.html { redirect_to checks_url, notice: 'Check was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_check
+      @check = Check.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def check_params
+      params.require(:check).permit(:name)
+    end
+end
---
+++ config/initializers/vision.rb
@@ -0,0 +1,16 @@
+require "google/cloud/vision"
+GOOGLE_APPLICATION_CREDENTIALS = '/google_application_credentials.json'
+
+# Your Google Cloud Platform project ID
+project_id = "hopeful-flame-155502"
+
+scopes =  ['https://www.googleapis.com/auth/cloud-platform',
+           'https://www.googleapis.com/auth/compute']
+authorization = Google::Auth.get_application_default(scopes)
+
+# Add the the access token obtained using the authorization to a hash, e.g
+# headers.
+# some_headers = {}
+# authorization.apply(some_headers)
+# Instantiates a client
+VISION = Google::Cloud::Vision.new project: project_id
