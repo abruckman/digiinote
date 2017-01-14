0c9b5a12e94cee6a0b52436578a8fa9808c7c6d0
Sarav Shah <saravsshah@gmail.com>
2017-01-14 13:50:27 -0800

    Refresh token working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
+      user.image = auth.info.image
       user.oauth_token = auth.credentials.token
+      user.oauth_refresh_token = auth.credentials.refresh_token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
