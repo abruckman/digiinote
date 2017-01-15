a2a6db02b88789de28408943ff85f0369474e671
Sarav Shah <saravsshah@gmail.com>
2017-01-14 19:17:07 -0800

    Add log in link for google drive

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,7 @@ class SessionsController < ApplicationController
 		 ],
 		 redirect_uri: "http://localhost:3000/oauth2callback")
 		auth_url = credentials.authorization_uri
-		p auth_url
-		p "^v"*100
+		
 		# byebug
 		redirect_to(auth_url.to_s)
 
@@ -33,11 +32,12 @@ class SessionsController < ApplicationController
 		credentials.code = params['code']
 		credentials.fetch_access_token!
 		@session = GoogleDrive::Session.from_credentials(credentials)
+		p "-----" * 100
+		p @session
+		p "-----" * 100
 		
-		session.files.each do |file|
-			p file.title
-		end 
-		@files = session.files
+
+		@files = @session.files
 		redirect_to '/notes'
 		 
 	end
