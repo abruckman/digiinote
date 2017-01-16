ee03a6eb6debe63dcf6d667b98bd7551df5d5daf
valeriamartinez94 <valeriamartinez94@gmail.com>
2017-01-15 17:24:12 -0800

    Add error handling

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   def index
+
   end
 
   def new
@@ -7,27 +8,33 @@ class NotesController < ApplicationController
 
   def show
     @id = params[:id]
+    p "note show page-------------"
+
   end
 
   def create
-    scanned = VISION.image(params[:picture]).text
-    "*" * 50
-    text = scanned.text
-    p "%" *50
-    Note.create({text: text})
-    credentials = Google::Auth::UserRefreshCredentials.new(
-     client_id: ENV['OAUTH'],
-     client_secret: ENV['CLIENT_SECRET'],
-     scope: [
-       "https://www.googleapis.com/auth/drive",
-       "https://spreadsheets.google.com/feeds/",
-     ],
-     redirect_uri: "http://localhost:3000/oauth2callback")
-    auth_url = credentials.authorization_uri
+    begin
+        scanned = VISION.image(params[:picture]).text
+        "*" * 50
+        text = scanned.text
+        p "%" *50
+        Note.create({text: text})
+        credentials = Google::Auth::UserRefreshCredentials.new(
+         client_id: ENV['OAUTH'],
+         client_secret: ENV['CLIENT_SECRET'],
+         scope: [
+           "https://www.googleapis.com/auth/drive",
+           "https://spreadsheets.google.com/feeds/",
+         ],
+         redirect_uri: "http://localhost:3000/oauth2callback")
+        auth_url = credentials.authorization_uri
+        redirect_to(auth_url.to_s)
 
-    # byebug
-    redirect_to(auth_url.to_s)
-
+        # byebug
+    rescue
+      flash[:notice] = "Something went wrong, try again"
+      redirect_to notes_path
+    end
     # redirect_to '/notes'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
 		 ],
 		 redirect_uri: "http://localhost:3000/oauth2callback")
 		auth_url = credentials.authorization_uri
-
 		# byebug
 		redirect_to(auth_url.to_s)
 
@@ -20,26 +19,29 @@ class SessionsController < ApplicationController
 
 
 	def create
-		credentials = Google::Auth::UserRefreshCredentials.new(
-			client_id: ENV['OAUTH'],
-			client_secret: ENV['CLIENT_SECRET'],
-			scope: [
-			  "https://www.googleapis.com/auth/drive",
-			  "https://spreadsheets.google.com/feeds/",
-			],
-			redirect_uri: "http://localhost:3000/oauth2callback")
-		credentials.code = params['code']
-		credentials.fetch_access_token!
-		# session[:access_token] = credentials.refresh_token
-		session = GoogleDrive.login_with_oauth(credentials)
-		p "-----to s" * 100
-		# session[:authorization] = session1.authorization
-		# p credentials.authorization
-		# p session1.authorization
-		@note = Note.last
-		@document = session.upload_from_string(@note.text, title='FUCKATHIS', :content_type => "text/plain")
-		# p "document -----------------"
-		# p @document
+
+				credentials = Google::Auth::UserRefreshCredentials.new(
+					client_id: ENV['OAUTH'],
+					client_secret: ENV['CLIENT_SECRET'],
+					scope: [
+					  "https://www.googleapis.com/auth/drive",
+					  "https://spreadsheets.google.com/feeds/",
+					],
+					redirect_uri: "http://localhost:3000/oauth2callback")
+				credentials.code = params['code']
+				credentials.fetch_access_token!
+				# session[:access_token] = credentials.refresh_token
+				session = GoogleDrive.login_with_oauth(credentials)
+				p "-----to s" * 100
+				# session[:authorization] = session1.authorization
+				# p credentials.authorization
+				# p session1.authorization
+				@note = Note.last
+				@document = session.upload_from_string(@note.text, title='FUCKATHIS', :content_type => "text/plain")
+
+				# p "document -----------------"
+				# p @documen
+
 
 
 		redirect_to "/notes/#{@document.id}"
