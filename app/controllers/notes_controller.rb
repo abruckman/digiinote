479c9050768bc7a7d373eb97c7046703f88f0bda
Andrew Bruckman <abruckman09@gmail.com>
2017-01-16 13:06:45 -0800

    fixed routes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -35,7 +35,9 @@ class NotesController < ApplicationController
 
 
         text = scanned.text
-        Note.create({text: text, title: titleir})
+        p text
+        p "8"*50
+        Note.create({text: text, title: title})
         credentials = Google::Auth::UserRefreshCredentials.new(
          client_id: ENV['OAUTH'],
          client_secret: ENV['CLIENT_SECRET'],
@@ -55,5 +57,32 @@ class NotesController < ApplicationController
       redirect_to notes_path
     end
     # redirect_to '/notes'
+  end
+
+  def create_callback
+    credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id: ENV['OAUTH'],
+      client_secret: ENV['CLIENT_SECRET'],
+      scope: [
+        "https://www.googleapis.com/auth/drive",
+        "https://spreadsheets.google.com/feeds/",
+      ],
+      redirect_uri: BASE_URL )
+    credentials.code = params['code']
+    credentials.fetch_access_token!
+    # session[:access_token] = credentials.refresh_token
+    session = GoogleDrive.login_with_oauth(credentials)
+    p "-----to s" * 100
+    # session[:authorization] = session1.authorization
+    # p credentials.authorization
+    # p session1.authorization
+    @note = Note.last
+    @document = session.upload_from_string(@note.text, @note.title, :content_type => "text/plain")
+
+    # p "document -----------------"
+    # p @documen
+
+    redirect_to "/notes/#{@document.id}"
+
   end
 end
