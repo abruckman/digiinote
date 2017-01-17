694905586d46d9cb09fb82037275e670358b3d6c
Sarav Shah <saravsshah@gmail.com>
2017-01-17 15:47:12 -0800

    Added spec tests

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -27,67 +27,15 @@ RSpec.describe NotesController, type: :controller do
     it "renders new form" do 
       get :index
       response.should render_template(:partial => '_new')
-
     end
 
+    it "works!" do 
+      get :index
+      response.status.should be(200)
+    end
   end
 
-  # describe "GET #new" do 
-  #   it "click take a picture and hides form " do
+  describe 'GET '
 
-  #     click_link('Take a Picture')
-  #     expect(response).to render_template("new")
-  #   end
-  # end
-
-  # describe "GET #show" do
-  #   it "assigns the requested pet as @pet" do
-  #     pet = Pet.create! valid_attributes
-  #     get :show, params: {id: pet.to_param}
-  #     expect(assigns(:pet)).to eq(pet)
-  #   end
-  # end
-
-  # describe "GET #edit" do
-  #   it "assigns the requested pet as @pet" do
-  #     pet = Pet.create! valid_attributes
-  #     get :edit, params: {id: pet.to_param}
-  #     expect(assigns(:pet)).to eq(pet)
-  #   end
-  # end
-
-  # describe "PUT #update" do
-  #   context "with valid params" do
-  #     let(:new_attributes) {
-  #       {name: "TonyTheTiger", breed: "GrrreAT", age: 48, cute: true}
-  #     }
-
-  #     it "assigns the requested pet as @pet" do
-  #       pet = Pet.create! valid_attributes
-  #       put :update, params: {id: pet.to_param, pet: new_attributes}
-  #       expect(assigns(:pet)).to eq(pet)
-  #     end
-
-  #     it "redirects to the pet" do
-  #       pet = Pet.create! valid_attributes
-  #       put :update, params: {id: pet.to_param, pet: new_attributes}
-  #       expect(response).to redirect_to(pet)
-  #     end
-  #   end
-
-  #   context "with invalid params" do
-  #     it "assigns the pet as @pet" do
-  #       pet = Pet.create! valid_attributes
-  #       put :update, params: {id: pet.to_param, pet: invalid_attributes}
-  #       expect(assigns(:pet)).to eq(pet)
-  #     end
-
-  #     it "re-renders the 'edit' template" do
-  #       pet = Pet.create! valid_attributes
-  #       put :update, params: {id: pet.to_param, pet: invalid_attributes}
-  #       expect(response).to render_template("edit")
-  #     end
-  #   end
-  # end
-
+ 
 end
