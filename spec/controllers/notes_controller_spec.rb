33965c6781f9349eb90bc9f127fd842051f793e3
Sarav Shah <saravsshah@gmail.com>
2017-01-17 16:48:05 -0800

    Add more spec tests

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -35,7 +35,31 @@ RSpec.describe NotesController, type: :controller do
     end
   end
 
-  describe 'GET '
+  describe "GET#oauth2" do 
+    let(:credentials) { Google::Auth::UserRefreshCredentials.new(
+     client_id: ENV['OAUTH'],
+     client_secret: ENV['CLIENT_SECRET'],
+     scope: [
+       "https://www.googleapis.com/auth/drive",
+       "https://spreadsheets.google.com/feeds/",
+
+     ],
+     redirect_uri:'http://localhost:3000/oauth2callback'
+ ) }
+
+    it "hit the oauth2 route" do 
+      auth_url = credentials.authorization_uri
+
+      get :save_google
+      expect(response).to redirect_to(auth_url.to_s)
+
+    end
+
+  end
+
+
+  
+
 
  
 end
